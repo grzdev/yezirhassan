9ae920c6886af8baf296a88f7821195ce383b6e5
zay <damilolaoyeniyi13@gmail.com>
2023-10-30 00:10:54 +0100

    First commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 
   import "../app.css";
   import Navbar from "../components/Navbar.svelte";
+  import Home from "../modules/Home.svelte";
   let menuOpen = false;
 </script>
 
 <div>
   <Navbar {menuOpen} on:toggleMenu={() => (menuOpen = !menuOpen)} />
+  <Home />
 </div>
 
 <style>
