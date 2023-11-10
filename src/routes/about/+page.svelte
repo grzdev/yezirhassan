fe8dccb3d2c86330efa68433c3da9ab7501d4b4f
zay <damilolaoyeniyi13@gmail.com>
2023-11-10 18:33:47 +0100

    Second div

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,22 @@
 <script>
   import Page from "$lib/Page.svelte";
+  import LoadScreen from "../../components/LoadScreen.svelte";
   import Navbar from "../../components/Navbar.svelte";
   import About from "../../modules/About.svelte";
   let menuOpen = false;
+
+  // LoadScreen
+  let isLoading = true;
+  setTimeout(() => {
+    isLoading = false;
+  }, 2000);
 </script>
 
-<Page>
-  <Navbar {menuOpen} on:toggleMenu={() => (menuOpen = !menuOpen)} />
-  <About />
-</Page>
+{#if isLoading}
+  <LoadScreen />
+{:else}
+  <div>
+    <Navbar {menuOpen} on:toggleMenu={() => (menuOpen = !menuOpen)} />
+    <About />
+  </div>
+{/if}
